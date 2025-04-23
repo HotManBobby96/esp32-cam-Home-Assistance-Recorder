7ba142603f8702adf93c8bfcee7eb59f9e27775d
Bryson Blakney <beblakney2008@gmail.com>
2025-04-23 08:13:48 -0500

    Server Changes
    Added a server to the esp32 and a updated bleak script to find the server and communicate. Need to find a way to get it to take pictures when alerted to do so by home assitance

--- projectFiles/bleak.py
+++ projectFiles/bleak.py
@@ -1,14 +1,27 @@
 #importing nessacary libraries
 import asyncio
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 
-ADDRESS = "" # replace with your current MAC Adress
-CHAR_UUID = ""  # UUID from your sketch
+SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-async def main():
-    async with BleakClient(ADDRESS) as client:
-        await client.write_gatt_char(CHAR_UUID, b"record")
-        print("Trigger sent")
+async def main(): 
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
+    
+
+    address = input("enter MAC adress") # get this later from esp32 to know where it is at
+
+    # Thabk you stack overflow
+    async with BleakClient(address) as client:
+        connected = await client.is_connected()
+        print(f"Connected: {connected}")
+
+        value = await client.read_gatt_char(CHARACTERISTIC_UUID)
+        print(f"Read: {value.decode()}")
+
+        await client.write_gatt_char(CHARACTERISTIC_UUID, b"Hello from PC!")
+        print("Wrote: Hello from PC!")
 
 asyncio.run(main())
- 
