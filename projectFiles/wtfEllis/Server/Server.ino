10650ae1fc94dbf82300b546bfc3a1165aade993
Bryson Blakney <beblakney2008@gmail.com>
2025-04-30 11:46:18 -0500

    added camera ability
    the camera can take pictures and save them to the sd card I think. Almost eady to merge.

--- projectFiles/wtfEllis/Server/Server.ino
+++ projectFiles/wtfEllis/Server/Server.ino
@@ -1,9 +1,14 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "esp_camera.h"
+#include "FS.h"
+#include "SD_MMC.h"
+
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
 
 bool deviceConnected = false;
 String name = "esp32bryson";
@@ -26,6 +31,64 @@ class MyServerCallbacks : public BLEServerCallbacks {
 };
 
 // FUNCTIONS
+
+void startCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    while (true);
+  }
+}
+
+void takePicture() {
+  if (!SD_MMC.begin()) {
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  File file = SD_MMC.open("/photo.jpg", FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+  } else {
+    file.write(fb->buf, fb->len); // Save image to SD card
+    Serial.println("Saved file to /photo.jpg");
+  }
+
+  file.close();
+  esp_camera_fb_return(fb);
+}
+
 void startServer() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
@@ -54,11 +117,16 @@ void startServer() {
 // SETUP & LOOP
 void setup() {
   startServer(); 
+
+  // camera setup
+  startCamera();
+
 }
 
 void loop() {
   if (deviceConnected) {
     Serial.println("Connected");
+    // takePicture();
   } else {
     Serial.println("Waiting for connection");
   }
